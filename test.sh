330421ff11919e8f54fa8ebc5ca4024932028818
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2026-01-22 17:33:04 +0530

    GCP again change to scirpt to dynamic to improvise

--- test.sh
+++ test.sh
@@ -5,6 +5,8 @@ cat k8s.tf
 terraform init
 export REGION=$(gcloud config get-value compute/region)
 export ZONE=$(gcloud config get-value compute/zone)
+gcloud config set compute/region  $REGION
+gcloud config set compute/zone $ZONE
 echo $REGION
 echo $ZONE
 terraform apply -var="region=${REGION}" -var="location=${ZONE}"
