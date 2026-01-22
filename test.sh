b1fbd8af18a4a6106cbfd06fcacebdfeea767d1c
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2026-01-22 17:11:36 +0530

    1st GCP script commit

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
-#!bin/bash
+gsutil -m cp -r gs://spls/gsp233/* .
+cd tf-gke-k8s-service-lb
+cat main.tf
+cat k8s.tf
+REGION=$(gcloud config get compute/region)
+ZONE=$(gcloud config get compute/zone)
+echo $REGION
+echo $ZONE
+terraform init
+terraform apply -var="region="${REGION}"" -var="location="${ZONE}""
 
-#creating the scripting starting with HASHBANG
-This is the first linux with git command practice.
