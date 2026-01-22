f4fac32bf0872c8d9791d064f3b8a46db823d736
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2026-01-22 17:19:47 +0530

    GCP region change for not working at first

--- test.sh
+++ test.sh
@@ -2,10 +2,5 @@ gsutil -m cp -r gs://spls/gsp233/* .
 cd tf-gke-k8s-service-lb
 cat main.tf
 cat k8s.tf
-REGION=$(gcloud config get compute/region)
-ZONE=$(gcloud config get compute/zone)
-echo $REGION
-echo $ZONE
 terraform init
-terraform apply -var="region="${REGION}"" -var="location="${ZONE}""
-
+terraform apply -var="region=europe-west4" -var="location=europe-west4-a"
