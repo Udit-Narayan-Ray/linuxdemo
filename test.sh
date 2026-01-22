c3a0c2def901258d0ad49a2244f507b5ce5df5ed
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2026-01-22 17:30:14 +0530

    GCP again change the scipt to dynamic not changed prviously

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@ cd tf-gke-k8s-service-lb
 cat main.tf
 cat k8s.tf
 terraform init
-REGION=$(gcloud config get-value compute/region)
-ZONE=$(gcloud config get-value compute/zone)
+export REGION=$(gcloud config get-value compute/region)
+export ZONE=$(gcloud config get-value compute/zone)
 echo $REGION
 echo $ZONE
 terraform apply -var="region=${REGION}" -var="location=${ZONE}"
