aa58b8a68527113a512d7dab683f3e8c88839611
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2026-01-22 17:35:14 +0530

    GCP lastly pasted zone and region manually no dynamically

--- test.sh
+++ test.sh
@@ -3,10 +3,4 @@ cd tf-gke-k8s-service-lb
 cat main.tf
 cat k8s.tf
 terraform init
-export REGION=$(gcloud config get-value compute/region)
-export ZONE=$(gcloud config get-value compute/zone)
-gcloud config set compute/region  $REGION
-gcloud config set compute/zone $ZONE
-echo $REGION
-echo $ZONE
-terraform apply -var="region=${REGION}" -var="location=${ZONE}"
+terraform apply -var="region=us-central1" -var="location=us-central1-b"
